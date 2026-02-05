c362dc2c448e757bdf67eff2d265c166217a54f8
Tim Öhlenschläger <tim.oehlenschlaeger@gmx.de>
2026-02-05 10:34:11 +0100

    minimal iteration through known nodes

--- auto_apms_behavior_codec/src/dictionary_manager.cpp
+++ auto_apms_behavior_codec/src/dictionary_manager.cpp
@@ -2,6 +2,7 @@
 
 #include "auto_apms_behavior_tree_core/node/node_manifest.hpp"
 #include <set>
+#include <iostream>
 
 using namespace auto_apms_behavior_codec;
 
@@ -13,6 +14,26 @@ bool DictionaryManager::build_dictionary()
   // for debugging, print the number of known node types -> currently only 0 -> why?
   std::cout << "Building dictionary with " << known_node_ids.size() << " known node IDs." << std::endl;
 
+  // iterate through all node ids and get their models
+  uint32_t id_counter = 0;
+  for (const auto & node_id : known_node_ids) {
+    try {
+      auto manifest = auto_apms_behavior_tree::core::NodeManifest::fromResource(node_id);
+      std::vector<std::string> node_names= manifest.getNodeNames();
+      for (const auto & node_name : node_names) {
+        DictionaryInfo info;
+        info.id = id_counter++;
+        info.name = node_name;
+        dictionary_map_[info.name] = info;
+        //todo parameters
+        std::cout << "Added node to dictionary: " << info.name << " with ID " << info.id << std::endl;
+      }
+    } catch (const std::exception & e) {
+      std::cerr << "Error loading node manifest " << std::endl;
+    }
+  }
+
+  //next get node modells for all ids
   return false;
 }
 
@@ -21,3 +42,6 @@ DictionaryManager::DictionaryManager()
   //constructor must build the dictionary
   build_dictionary();
 }
+
+DictionaryInfo::DictionaryInfo()
+{}
