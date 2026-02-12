50d2b46725b278d32881f303f3b6fc9d27c42438
Tim Öhlenschläger <tim.oehlenschlaeger@gmx.de>
2026-02-12 09:49:12 +0100

    at BT::anyType allowed as supported type, and define classes to represent ports

--- auto_apms_behavior_codec/include/behavior_tree_representation.hpp
+++ auto_apms_behavior_codec/include/behavior_tree_representation.hpp
@@ -12,10 +12,46 @@ struct Port {
 };
 
 struct PortInt : public Port {
-  explicit PortInt(int32_t v) : value(v) {}
-  uint16_t getID() const override { return 0; }
+  explicit PortInt(int32_t v, int16_t id) : value(v), id(id) {}
+  uint16_t getID() const override { return this->id; }
   int32_t value;
+  private:
+  int16_t id; //id of the port, this is its index in the vector of ports for the node, this is may differ between ports with same name/type, if they belong to different nodes
 };
+
+struct PortFloat : public Port {
+  explicit PortFloat(float v, int16_t id) : value(v), id(id) {}
+  uint16_t getID() const override { return this->id; }
+  float value;
+  private:
+  int16_t id; //id of the port, this is its index in the vector of ports for the node, this is may differ between ports with same name/type, if they belong to different nodes
+};
+
+struct PortString : public Port {
+  explicit PortString(const std::string& v, int16_t id) : value(v), id(id) {}
+  uint16_t getID() const override { return this->id; }
+  std::string value;
+  private:
+  int16_t id; //id of the port, this is its index in the vector of ports for the node, this is may differ between ports with same name/type, if they belong to different nodes
+};
+
+struct PortBool : public Port {
+  explicit PortBool(bool v, int16_t id) : value(v), id(id) {}
+  uint16_t getID() const override { return this->id; }
+  bool value;
+  private:
+  int16_t id; //id of the port, this is its index in the vector of ports for the node, this is may differ between ports with same name/type, if they belong to different nodes
+};
+
+struct PortAnyTypeAllowed : public Port {
+  explicit PortAnyTypeAllowed(const BT::AnyTypeAllowed& v, int16_t id) : value(v), id(id) {}
+  uint16_t getID() const override { return this->id; }
+  BT::AnyTypeAllowed value;
+  private:
+  int16_t id; //id of the port, this is its index in the vector of ports for the node, this is may differ between ports with same name/type, if they belong to different nodes
+};
+
+
 
 struct Node {
   std::string registration_name;
